90d1cbdfdc7149851ca2819f103d14b55409ac39
ashish <ashish.gec@gmail.com>
2013-10-27 02:03:12 +0530

    updated css

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var io = require('socket.io').listen(app.listen(port));
 
 
 io.sockets.on('connection', function (socket) {
-    socket.emit('message', { message: 'welcome to the chat' });
+    socket.emit('message', { message: 'welcome to the AptChat' });
     socket.on('send', function (data) {
         io.sockets.emit('message', data);
     });
--- public/chat.js
+++ public/chat.js
@@ -14,7 +14,7 @@ window.onload = function() {
             messages.push(data);
             var html = '';
             for(var i=0; i<messages.length; i++) {
-                html += '<b>' + (messages[i].username ? messages[i].username : 'AptChat') + ': </b>';
+                html += '<b>' + (messages[i].username ? messages[i].username : 'God') + ': </b>';
                 html += messages[i].message + '<br />';
             }
             content.innerHTML = html;
